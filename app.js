959bf1c59972852e0f8c7a3851a20bad87830a63
Erik Vorkink <erik@vorkink.com>
2017-09-17 23:39:59 -0600

    Converted a few things to ES6 by pulling in Babel's convert-in-the-browser tool

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function(){
-	"use strict";
+	'use strict';
 
-	var PITCHES = [
+	const PITCHES = [
 		{ name: 'A', audio: 'freesound.org/4409__pinkyfinger__piano-notes-1-octave/68437__pinkyfinger__piano-a.wav' },
 		{ name: 'Bb', audio: 'freesound.org/4409__pinkyfinger__piano-notes-1-octave/68439__pinkyfinger__piano-bb.wav' },
 		{ name: 'B', audio: 'freesound.org/4409__pinkyfinger__piano-notes-1-octave/68438__pinkyfinger__piano-b.wav' },
@@ -16,11 +16,11 @@
 		{ name: 'G#', audio: 'freesound.org/4409__pinkyfinger__piano-notes-1-octave/68448__pinkyfinger__piano-g.wav' }
 	];
 
-	var PITCHES_PER_ROUND = 5;
+	const PITCHES_PER_ROUND = 5;
 
-	var roundState = null;
+	let roundState = null;
 
-	function startRound() {
+	let startRound = () => {
 		roundState = {
 			currentPitch: null,
 			numCorrect: 0,
@@ -31,7 +31,7 @@
 
 		// Choose random pitches to be used for this round
 		while (roundState.pitches.length < PITCHES_PER_ROUND) {
-			var pitch = PITCHES[Math.floor(Math.random() * PITCHES.length)];
+			let pitch = PITCHES[Math.floor(Math.random() * PITCHES.length)];
 			if (roundState.pitches.indexOf(pitch) === -1) {
 				roundState.pitches.push(pitch);
 			}
@@ -42,25 +42,25 @@
 		document.getElementById('round-content').style.display = 'block';
 
 		askNextQuestion();
-	}
+	};
 
-	function askNextQuestion() {
-		var nextPitch = roundState.pitches[roundState.responses.length];
+	let askNextQuestion = () => {
+		let nextPitch = roundState.pitches[roundState.responses.length];
 		if (nextPitch) {
 			askQuestion(nextPitch);
 		} else {
 			finishRound();
 		}
-	}
+	};
 
-	function askQuestion(pitch) {
-		var num = roundState.responses.length + 1;
-		document.getElementById('question').innerHTML = 'Question ' + num + ': Can you click ' + pitch.name + '?';
+	let askQuestion = (pitch) => {
+		let num = roundState.responses.length + 1;
+		document.getElementById('question').innerHTML = `Question ${num}: Can you click ${pitch.name}?`;
 		roundState.currentPitch = pitch;
 		playQuestion(pitch);
-	}
+	};
 
-	function evalAnswer(answer) {
+	let evalAnswer = (answer) => {
 		if (!roundState.currentPitch) {
 			alert('Not ready to accept a response.');
 			return;
@@ -74,22 +74,22 @@
 
 		updateScoreboard();
 		askNextQuestion();
-	}
+	};
 
-	function updateScoreboard() {
-		var correct = roundState ? roundState.numCorrect : 'n/a';
-		var incorrect = roundState ? roundState.numIncorrect : 'n/a';
+	let updateScoreboard = () => {
+		let correct = roundState ? roundState.numCorrect : 'n/a';
+		let incorrect = roundState ? roundState.numIncorrect : 'n/a';
 		document.getElementById('num-correct').innerHTML = correct;
 		document.getElementById('num-incorrect').innerHTML = incorrect;
-	}
+	};
 
-	function finishRound() {
-		document.getElementById('last-score').innerHTML = 'Your score is ' + roundState.numCorrect + ' / ' + roundState.pitches.length;
+	let finishRound = () => {
+		document.getElementById('last-score').innerHTML = `Your score is ${roundState.numCorrect} / roundState.pitches.length`;
 		document.getElementById('round-content').style.display = 'none';
 
-		var percentCorrect = (roundState.numCorrect / roundState.pitches.length) * 100;
+		let percentCorrect = (roundState.numCorrect / roundState.pitches.length) * 100;
 
-		var sound;
+		let sound;
 		if (percentCorrect === 100) {
 			sound = 'freesoundeffects.com/cheer.wav';
 		} else if (percentCorrect >= 70) {
@@ -103,44 +103,44 @@
 			// or boohiss.wav
 			// or boos3.wav
 		}
-		var url = 'audio/' + sound;
+		let url = 'audio/' + sound;
 		playURL(url);
-	}
+	};
 
-	function initClickHandlers() {
+	let initClickHandlers = () => {
 		document.getElementById('start-round-btn').addEventListener('click', startRound);
 
-		var buttons = document.getElementsByClassName('response-btn');
-		for (var i = 0; i < buttons.length; i++) {
+		let buttons = document.getElementsByClassName('response-btn');
+		for (let i = 0; i < buttons.length; i++) {
 			buttons[i].addEventListener('click', onResponseBtnClick, false);
 		}
-	}
+	};
 
-	function onResponseBtnClick(event) {
+	let onResponseBtnClick = (event) => {
 		if (!event.target) {
 			alert('Invalid response.');
 			return;
 		}
-		var response = event.target.getAttribute('data-response');
+		let response = event.target.getAttribute('data-response');
 		if (!response) {
 			alert('Invalid response.');
 			return;
 		}
 
 		evalAnswer(response);
-	}
+	};
 
-	function playURL(url) {
-		var sound = new Howl({
+	let playURL = (url) => {
+		let sound = new Howl({
 			src: [url]
 		});
 		sound.play();
-	}
+	};
 
-	function playQuestion(pitch) {
-		var url = 'audio/' + pitch.audio;
+	let playQuestion = (pitch) => {
+		let url = 'audio/' + pitch.audio;
 		playURL(url);
-	}
+	};
 
 	initClickHandlers();
 
